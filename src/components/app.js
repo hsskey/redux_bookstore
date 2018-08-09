2515c27b07c3ef0dc48ec1c34e6bfbb9fad3a7a7
hsskey <slingshot800@gmail.com>
2018-08-09 16:19:28 +0900

    commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import BookList from './book-list'
+import BookList from '../containers/book-list'
 export default class App extends Component {
   render() {
     return (
